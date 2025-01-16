a843716dcb8333fb28035108a1556ddd8dff692c
Gandhi.R <gandhi.raketla@gmail.com>
2025-01-15 23:45:02 -0600

    Added front end

--- backend/api/lead_generation_api.py
+++ backend/api/lead_generation_api.py
@@ -13,6 +13,7 @@ from services.read_json_test import JSONFileReader
 from agent.lead_generation_crew import ResearchCrew
 import json
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 class QueryRequest(BaseModel):
     query: str
@@ -21,27 +22,32 @@ class LeadGenerationAPI:
     def __init__(self):
         self.app = FastAPI()
         self.prompt_extractor = UserPromptExtractor()
-        
+        self.app.add_middleware(CORSMiddleware,
+            allow_origins=["http://localhost:5173"],  # Frontend URL
+            allow_credentials=True,
+            allow_methods=["*"],  # Allows all methods
+            allow_headers=["*"],  # Allows all headers
+        )
         @self.app.post("/research")
         def execute_research(request: QueryRequest):
             # Extract structured info from user query
-            #extracted_json = self.prompt_extractor.extract_lead_info(request.query)
-            #print(extracted_json)
+            extracted_json = self.prompt_extractor.extract_lead_info(request.query)
+           # print(extracted_json)
             # Define callback for UI status updates
             #def example_task_callback(status: str):
                 # This can be modified to send updates to UI
                 #print(status)
             # Initialize research crew with callback
-            #crew = ResearchCrew()
+            crew = ResearchCrew()
             
             # Execute research with extracted JSON
-            #results = crew.execute_research(extracted_json)
-           # structured_json = json.loads(results)
-            #json.dumps(structured_json, indent=4)
+            results = crew.execute_research(extracted_json)
+            structured_json = json.loads(results)
+            json.dumps(structured_json, indent=4) 
             #response = JSONResponse(content=results)
             #print("Serialized Response Content:", response.body.decode())
             #print(structured_json)
-            structured_json=JSONFileReader().read_json()
+            #structured_json=JSONFileReader().read_json()
             return structured_json
 
 def create_app():
